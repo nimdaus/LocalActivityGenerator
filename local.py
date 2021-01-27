0d01f1879b4f6d7dda1140e4ee5967000266dca2
nimdaus <joshuaplambert@gmail.com>
2021-01-27 13:42:45 -0500

    Update local.py

--- local.py
+++ local.py
@@ -5,6 +5,7 @@ import psutil
 import numpy as np
 import os
 import subprocess
+from subprocess import Popen, CREATE_NEW_CONSOLE
 import json
 
 ip_address_port = 'http://127.0.0.1:5000/'
@@ -33,7 +34,8 @@ else:
 
 if os.path.exists(f"{os.getcwd()}"+"/Binary/data-generator.exe"):
 	print("Found Binary... running.")
-	subprocess.call([f"{os.getcwd()}"+"/Binary/data-generator.exe"], shell=True)
+	Popen(f"{os.getcwd()}"+"/Binary/data-generator.exe", creationflags=CREATE_NEW_CONSOLE)
+	#subprocess.call([f"{os.getcwd()}"+"/Binary/data-generator.exe"], shell=True)
 else:
 	print("Missing Generator!")
 
@@ -45,12 +47,13 @@ while True:
 	try:
 		for disk in psutil.disk_partitions():
 			if 'cdrom' in disk.opts:
-				continue
+				counter += 1
+				pass
 			elif disk.mountpoint in skip_drives:
-				continue
+				counter += 1
+				pass
 			else:
-				coutner =+ 1
-				print("ready")
+				counter += 1
 				print(psutil.disk_usage(disk.mountpoint).total)
 				disk_total = psutil.disk_usage(disk.mountpoint).total
 				disk_used = psutil.disk_usage(disk.mountpoint).used
@@ -63,41 +66,36 @@ while True:
 				print(list_of_add_rates)
 				new_add_rate_percent = random.choice(list_of_add_rates)
 				new_add = ((new_change_rate_percent / 100) * disk_used) / (1024 ** 3)
-				print("dicts")
 				volume_data[f"{disk.mountpoint}"] =  {"data_churn_rate": 0.1, "path": "scratch", "max_fragment_blocks": 512, "initial_usage": 1, "data_growth_rate": 1, "min_fragment_blocks": 128, "static_io_load": 1, "fragment": False, "database": {"updates_per_hour": 0, "queries_per_hour": 0, "inserts_per_hour": 0}}
 				volume_data[f"{disk.mountpoint}"]["data_churn_rate"] = new_change
 				volume_data[f"{disk.mountpoint}"]["initial_usage"] = set_used_space
 				volume_data[f"{disk.mountpoint}"]["data_growth_rate"] = new_add
 				print("fail")
 				if os.path.exists(f"{disk.mountpoint}"+"/scratch"):
-					print("don't use continue you tool")
 					pass
 				else:
 					os.mkdir(f"{disk.mountpoint}"+"/scratch")
 				profile["params"]["volumes"] = volume_data
-				print("volume")
 				list_of_cpu_load = np.linspace(minimum_cpu_load_percent, maximum_cpu_load_percent, number_of_change_profiles)
 				new_cpu_load = random.choice(list_of_cpu_load)
 				profile["params"]["cpu_load"] = new_cpu_load
 				list_of_ram_load = np.linspace(minimum_ram_load_percent, maximum_ram_load_percent, number_of_change_profiles)
 				new_ram_load = random.choice(list_of_ram_load)
 				profile["params"]["ram_load"] = new_ram_load
-				print("system")
-				if disk == psutil.disk_partitions()[-1]:
+				if counter == len(psutil.disk_partitions()):
+					counter = 0
 					profile["params"]["volumes"] = volume_data
-					print("Changing Usage Profile!")
 					response = requests.post(f"{ip_address_port}", headers=headers, data=str(profile))
 					time.sleep(1)
 					get = '{"id":1,"jsonrpc":"2.0","method":"get_config","params":{}}'
 					response = requests.post(f"{ip_address_port}", headers=headers, data=get)
 					if response.ok:
-						print("Success :)")
+						print(f"Success! Waiting {change_every_x_hours}Hours until next change")
 						volume_data = {}
 						time.sleep(change_every_x_hours*60*60)
 					else:
 						print("Bad Response :(")
 				else:
-					print("loop complete")
 					continue
 	except requests.RequestException as e:
 		print("Request Failed: %s" % e)
